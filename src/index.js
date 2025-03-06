7d88b930517d1e62e6f4d046e1e56f8fd5094a4c
Jon Schwartz <jonschwartz@Jons-MacBook-Pro.local>
2025-03-06 16:34:23 -0500

    Upgrade version of react and get app up-and-running

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 //component file
 import TodoContainer from "./components/TodoContainer";
 import "./App.css";
 
-ReactDOM.render(<TodoContainer />, document.getElementById("root"));
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <TodoContainer />
+  </StrictMode>
+);
